63fdb6b8acf8d1b743f396300322aa0207954b20
kohheepeace <kohheepeace@gmail.com>
2020-04-04 19:04:12 +0800

    Update title and description

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
             create: (context) => CountriesProvider()..fetchAllCountries()),
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Ytrendd',
         theme: ThemeData(
           primarySwatch: Colors.red,
         ),
